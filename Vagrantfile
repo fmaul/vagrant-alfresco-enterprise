8b081263225cc4b2afcddfa73182ec53d7a760d4
Florian Maul <f.maul@fme.de>
2017-03-24 17:07:59 +0100

    Updated scripts for Alfresco Enterprise 5.2

--- Vagrantfile
+++ Vagrantfile
@@ -8,14 +8,15 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu/wily64"
   config.vm.network "forwarded_port", guest: 21, host: 8021
   config.vm.network "forwarded_port", guest: 8080, host: 8080
+
   config.vm.provision "shell", path: "setup.sh"
 	
   config.vm.provider "virtualbox" do |vb|
      vb.customize ["modifyvm", :id, "--memory", "6144"]
-	 vb.customize ["modifyvm", :id, "--cpus", "8"]  
+	 vb.customize ["modifyvm", :id, "--cpus", "4"]   
      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
      vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]            
-     vb.customize ["modifyvm", :id, "--nictype1", "virtio"] 
+     vb.customize ["modifyvm", :id, "--nictype1", "virtio"]
   end	
   
 end
--- setup.sh
+++ setup.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
+echo ### Setting random passwords for root, vagrant and ubuntu user ###
+#PASS=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c12`
+PASS=vmware
+echo "ubuntu:$PASS" | chpasswd
+echo "root:$PASS" | chpasswd
+echo "vagrant:$PASS" | chpasswd
+
+echo ### Updating  ###
 apt-get update
 apt-get -y upgrade
 apt-get -y install fontconfig
 apt-get -y install curl
 apt-get -y install libXinerama1
 apt-get -y install libcups*
-apt-get -y install libglu1-mesa
- 
-releasename=alfresco-enterprise-5.0.1-installer-linux-x64.bin
+apt-get -y install libreoffice-base libreoffice-base-core libreoffice-base-drivers
+
+releasename=alfresco-content-services-installer-5.2.0-linux-x64.bin
 
 # Change working dir to vagrant directory
 cd /vagrant
 
 # Install Alfresco using the key file
-./$releasename < /vagrant/install-keys
+./$releasename --mode unattended --prefix /opt/alfresco --alfresco_admin_password admin --unattendedmodeui minimal
 
 # Copy extensions and amp modules
 cd /opt/alfresco
@@ -22,6 +30,7 @@ cp -Rv /vagrant/shared-classes/* tomcat/shared/classes/
 cp -Rv /vagrant/shared-lib/* tomcat/shared/lib/
 cp -Rv /vagrant/amps-repo/* amps/
 cp -Rv /vagrant/amps-share/* amps_share/
+mkdir -p tomcat/shared/classes/alfresco/extension/license/
 cp -v /vagrant/license/* tomcat/shared/classes/alfresco/extension/license/
 
 # Install amp modules into alfresco.war and share.war
@@ -33,3 +42,4 @@ $mmt list tomcat/webapps/share.war
 
 # Start Alfresco after installation
 service alfresco start 
+
