b60995bbd66cd464a2b81e9e10f0d4f763472c75
Florian Maul <f.maul@fme.de>
2019-03-28 10:36:42 +0100

    Update to 5.2.4

--- Vagrantfile
+++ Vagrantfile
@@ -5,15 +5,15 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "ubuntu/wily64"
+  config.vm.box = "ubuntu/xenial64"
   config.vm.network "forwarded_port", guest: 21, host: 8021
   config.vm.network "forwarded_port", guest: 8080, host: 8080
 
   config.vm.provision "shell", path: "setup.sh"
 	
   config.vm.provider "virtualbox" do |vb|
-     vb.customize ["modifyvm", :id, "--memory", "6144"]
-	 vb.customize ["modifyvm", :id, "--cpus", "4"]   
+     vb.customize ["modifyvm", :id, "--memory", "8192"]
+     vb.customize ["modifyvm", :id, "--cpus", "4"]   
      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
      vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]            
      vb.customize ["modifyvm", :id, "--nictype1", "virtio"]
--- setup.sh
+++ setup.sh
@@ -16,28 +16,29 @@ apt-get -y install libXinerama1
 apt-get -y install libcups*
 apt-get -y install libreoffice-base libreoffice-base-core libreoffice-base-drivers
 
-releasename=alfresco-content-services-installer-5.2.0-linux-x64.bin
+releasename=alfresco-content-services-installer-5.2.4-linux-x64.bin
 
 # Change working dir to vagrant directory
 cd /vagrant
 
 # Install Alfresco using the key file
+chmod 755 $releasename
 ./$releasename --mode unattended --prefix /opt/alfresco --alfresco_admin_password admin --unattendedmodeui minimal
 
 # Copy extensions and amp modules
 cd /opt/alfresco
-cp -Rv /vagrant/shared-classes/* tomcat/shared/classes/
-cp -Rv /vagrant/shared-lib/* tomcat/shared/lib/
-cp -Rv /vagrant/amps-repo/* amps/
-cp -Rv /vagrant/amps-share/* amps_share/
+cp -Rv /vagrant/shared-classes/* tomcat/shared/classes/ 2>/dev/null
+cp -Rv /vagrant/shared-lib/* tomcat/shared/lib/ 2>/dev/null
+cp -Rv /vagrant/amps-repo/* amps/ 2>/dev/null
+cp -Rv /vagrant/amps-share/* amps_share/ 2>/dev/null
 mkdir -p tomcat/shared/classes/alfresco/extension/license/
 cp -v /vagrant/license/* tomcat/shared/classes/alfresco/extension/license/
 
 # Install amp modules into alfresco.war and share.war
 mmt="/opt/alfresco/java/bin/java -jar /opt/alfresco/bin/alfresco-mmt.jar"
-$mmt install -force amps tomcat/webapps/alfresco.war -directory $*
+$mmt install amps tomcat/webapps/alfresco.war -directory $*
 $mmt list tomcat/webapps/alfresco.war
-$mmt install -force amps_share tomcat/webapps/share.war -directory $*
+$mmt install amps_share tomcat/webapps/share.war -directory $*
 $mmt list tomcat/webapps/share.war
 
 # Start Alfresco after installation
